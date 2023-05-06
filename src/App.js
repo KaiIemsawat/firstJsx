d3d68cc41def12fa4bb58bce3002d4ccfaa7a98a
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-05-05 20:50:08 -0400

    add a note

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import TodoHeader from "./components/TodoHeader";
 import TodoList from "./components/TodoList";
 
+// I am aware that I don't need to create/import two jsx files. This is just an experiment and it works
 function App() {
   return (
     <div className="App">
