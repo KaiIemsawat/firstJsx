d5b9a20bb7284215f63ef6bdf578670cefae16ec
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-05-05 20:47:59 -0400

    first jsx

---
+++ src/components/TodoHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoHeader = () => {
+    return (
+        <>
+            <h1>Hello Dojo!!</h1>
+            <h3>Things I need to do :</h3>
+        </>
+    )
+}
+
+export default TodoHeader;
